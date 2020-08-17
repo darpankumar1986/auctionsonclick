5613cb55dc2ccca74c2a63707be3fe9b0507aa7a
akashsharma13 <58677901+akashsharma13@users.noreply.github.com>
2020-08-17 15:41:59 +0530

    add class
    add class

--- application/views/front_view/auctionDetail.php
+++ application/views/front_view/auctionDetail.php
@@ -1,5 +1,5 @@
-	<script src="<?php echo base_url(); ?>assets/auctiononclick/js/bootstrap.min.js?rand=<?php echo CACHE_RANDOM; ?>"></script>
-	<div class="container-fluid container_margin">
+    <script src="<?php echo base_url(); ?>assets/auctiononclick/js/bootstrap.min.js?rand=<?php echo CACHE_RANDOM; ?>"></script>
+    <div class="container-fluid container_margin">
             <div class="row row_bg">
                <div class="container">
                 <div class="col-sm-12">
@@ -17,7 +17,7 @@
            <div class="container">
                <div class="row">
                    <div class="col-sm-12">
-                       <div class="form_wrap_anction_search form-wrap">
+                       <div class="form_wrap_anction_search form-wrap form_dropdown_border">
                            <form class="form_desc">
                                <div class="dropdown">
                                    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Category
@@ -94,7 +94,7 @@
                 </div><!--row-->
         </div><!--container-fluid-->
         <div class="container">
-		<?php //echo "<pre>";print_r($auction_data);die; ?>
+        <?php //echo "<pre>";print_r($auction_data);die; ?>
             <div class="row">
                 <div class="col-sm-9 border_right">
                     <div class="desc_wrapper">
@@ -126,18 +126,18 @@
                             <div class="clr"></div>
                         </div>
                     </div>
-					<?php
-						$userid=$this->session->userdata('id');
-						$indate =	GetTitleByField('tbl_user_registration', "id='".$userid."'", "indate");
-						$free_sub_expire_date = date('Y-m-d H:i:s',strtotime(FREE_SUBSCRIPTION_TIME,strtotime($indate)));
-						$free_sub_expire_date_str = strtotime($free_sub_expire_date);
-						$free_sub_flag = 0;
-						if(time() < $free_sub_expire_date_str)
-						{
-							$free_sub_flag = 1;
-						}
+                    <?php
+                        $userid=$this->session->userdata('id');
+                        $indate =	GetTitleByField('tbl_user_registration', "id='".$userid."'", "indate");
+                        $free_sub_expire_date = date('Y-m-d H:i:s',strtotime(FREE_SUBSCRIPTION_TIME,strtotime($indate)));
+                        $free_sub_expire_date_str = strtotime($free_sub_expire_date);
+                        $free_sub_flag = 0;
+                        if(time() < $free_sub_expire_date_str)
+                        {
+                            $free_sub_flag = 1;
+                        }
 
-					?>
+                    ?>
                     <div class="table_desc">
                         <table class="table table-bordered">
                             <tbody>
@@ -145,38 +145,38 @@
                                     <td>Bank Name</td>
                                     <td><?php echo $auction_data[0]->bank_name; ?></td>
                                 </tr>
-								<?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
-									<tr>
-										<td>Bank Branch</td>
-										<td><?php echo $auction_data[0]->branch_name; ?></td>
-									</tr>
-									<tr>
-										<td>Contact Details</td>
-										<td><?php echo $auction_data[0]->contact_person_details_1; ?></td>
-									</tr>
-									<tr>
-										<td>Auction Type</td>
-										<td><?php echo ($auction_data[0]->event_type=='drt')?'DRT':'Sarfaesi';?> Auction</td>
-									</tr>
-										<tr>
-										<td>Borrower Name</td>
-										<td><?php echo $auction_data[0]->borrower_name; ?></td>
-									</tr>
-									<tr>
-										<td>Asset Category</td>
-										<td><?php echo $auction_data[0]->category_name; ?></td>
-									</tr>
-								<?php } ?>
+                                <?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
+                                    <tr>
+                                        <td>Bank Branch</td>
+                                        <td><?php echo $auction_data[0]->branch_name; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Contact Details</td>
+                                        <td><?php echo $auction_data[0]->contact_person_details_1; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Auction Type</td>
+                                        <td><?php echo ($auction_data[0]->event_type=='drt')?'DRT':'Sarfaesi';?> Auction</td>
+                                    </tr>
+                                        <tr>
+                                        <td>Borrower Name</td>
+                                        <td><?php echo $auction_data[0]->borrower_name; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Asset Category</td>
+                                        <td><?php echo $auction_data[0]->category_name; ?></td>
+                                    </tr>
+                                <?php } ?>
                                 <tr>
                                     <td>Asset Type</td>
                                     <td><?php echo $auction_data[0]->sub_category_name; ?></td>
                                 </tr>
-								<?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
-									<tr>
-										<td>Asset Details</td>
-										<td><?php echo $auction_data[0]->PropertyDescription; ?></td>
-									</tr>
-								<?php } ?>
+                                <?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
+                                    <tr>
+                                        <td>Asset Details</td>
+                                        <td><?php echo $auction_data[0]->PropertyDescription; ?></td>
+                                    </tr>
+                                <?php } ?>
                                 <tr>
                                     <td>Asset Location</td>
                                     <td><?php echo $auction_data[0]->location_name; ?></td>
@@ -201,76 +201,76 @@
                                     <td>EMD Submission Last Date</td>
                                     <td><?php echo date('F d',strtotime($auction_data[0]->bid_last_date)); ?><sup><?php echo date('S',strtotime($auction_data[0]->bid_last_date)); ?></sup>, <?php echo date('Y',strtotime($auction_data[0]->bid_last_date)); ?></td>
                                 </tr>
-								<?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
-									<tr>
-										<td>Auction Start Date & Time</td>
-										<td><?php echo date('F d',strtotime($auction_data[0]->auction_start_date)); ?><sup><?php echo date('S',strtotime($auction_data[0]->auction_start_date)); ?></sup>, <?php echo date('Y',strtotime($auction_data[0]->auction_start_date)); ?></td>
-									</tr>
-									<tr>
-										<td>Auction End Date & Time</td>
-										<td><?php echo date('F d',strtotime($auction_data[0]->auction_end_date)); ?><sup><?php echo date('S',strtotime($auction_data[0]->auction_end_date)); ?></sup>, <?php echo date('Y',strtotime($auction_data[0]->auction_end_date)); ?></td>
-									</tr>
-									<tr>
-										<td>E-Auction Provider</td>
-										<td><?php echo $auction_data[0]->service_no; ?></td>
-									</tr>
-									<tr>
-										<td>E-Auction Website</td>
-										<td><a href="<?php echo 'https://'.$auction_data[0]->far; ?>" target="_blank"><?php echo $auction_data[0]->far; ?></a></td>
-									</tr>
-									<tr>
-										<td>Documents Available</td>
-										<td> 
-											<?php 
-											$picSrNo=1;
-											foreach($uploadedDocs as $key => $doc){ if($doc->upload_document_field_id==0 && $doc->status == 1){ ?>
-												<a href="/public/uploads/event_auction/<?php echo $doc->file_path; ?>" target="_blank"><?php echo 'Pic '.$picSrNo; ?></a><?php if($key+1 != count($uploadedDocs)){ ?>,<?php } ?>
-											<?php 
-												$picSrNo++;	
-											} } ?>
-											<br/>
-											<?php 
-											$dSrNo=1;
-											foreach($uploadedDocs as $key => $doc){ if($doc->upload_document_field_id > 0){ ?>
-												<a href="/public/uploads/event_auction/<?php echo $doc->file_path; ?>" target="_blank"><?php echo 'Doc '.$dSrNo; ?></a><?php if($key+1 != count($uploadedDocs)){ ?>,<?php } ?>
-											<?php
-												$dSrNo++;
-												} 
-											
-											} ?>
-										</td>
-									</tr>
-								<?php } ?>
-								<?php if(($auction_data[0]->isSub > 0) || $free_sub_flag == 1){ ?>
-								<!--<tr>
+                                <?php if($auction_data[0]->isSub > 0 || $free_sub_flag == 1){ ?>
+                                    <tr>
+                                        <td>Auction Start Date & Time</td>
+                                        <td><?php echo date('F d',strtotime($auction_data[0]->auction_start_date)); ?><sup><?php echo date('S',strtotime($auction_data[0]->auction_start_date)); ?></sup>, <?php echo date('Y',strtotime($auction_data[0]->auction_start_date)); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Auction End Date & Time</td>
+                                        <td><?php echo date('F d',strtotime($auction_data[0]->auction_end_date)); ?><sup><?php echo date('S',strtotime($auction_data[0]->auction_end_date)); ?></sup>, <?php echo date('Y',strtotime($auction_data[0]->auction_end_date)); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>E-Auction Provider</td>
+                                        <td><?php echo $auction_data[0]->service_no; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>E-Auction Website</td>
+                                        <td><a href="<?php echo 'https://'.$auction_data[0]->far; ?>" target="_blank"><?php echo $auction_data[0]->far; ?></a></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Documents Available</td>
+                                        <td>
+                                            <?php
+                                            $picSrNo=1;
+                                            foreach($uploadedDocs as $key => $doc){ if($doc->upload_document_field_id==0 && $doc->status == 1){ ?>
+                                                <a href="/public/uploads/event_auction/<?php echo $doc->file_path; ?>" target="_blank"><?php echo 'Pic '.$picSrNo; ?></a><?php if($key+1 != count($uploadedDocs)){ ?>,<?php } ?>
+                                            <?php
+                                                $picSrNo++;
+                                            } } ?>
+                                            <br/>
+                                            <?php
+                                            $dSrNo=1;
+                                            foreach($uploadedDocs as $key => $doc){ if($doc->upload_document_field_id > 0){ ?>
+                                                <a href="/public/uploads/event_auction/<?php echo $doc->file_path; ?>" target="_blank"><?php echo 'Doc '.$dSrNo; ?></a><?php if($key+1 != count($uploadedDocs)){ ?>,<?php } ?>
+                                            <?php
+                                                $dSrNo++;
+                                                }
+
+                                            } ?>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                                <?php if(($auction_data[0]->isSub > 0) || $free_sub_flag == 1){ ?>
+                                <!--<tr>
                                     <td colspan="2" class="contact_desc"><p>For further assistance, please contact our sales team</p>
                                         <p><?php echo $sales_person_detail; ?>
-										</p>
+                                        </p>
                                     </td>
                                 </tr>-->
                                 <tr>
                                     <td colspan="2" class="residential_btn"><button class="btn btn-default" type="button" onclick="window.location='<?php echo base_url(); ?>propertylisting?search=<?php echo $auction_data[0]->city_name; ?>&assetsTypeId=1'">View more residential plot in <?php echo $auction_data[0]->city_name; ?> <span><i class="fa fa-angle-right" aria-hidden="true"></i></span></button></td>
                                 </tr>
-								<?php } ?>
+                                <?php } ?>
                             </tbody>
                         </table>
                     </div>
-					<?php if(!($auction_data[0]->isSub > 0 || $free_sub_flag == 1)){ ?>
+                    <?php if(!($auction_data[0]->isSub > 0 || $free_sub_flag == 1)){ ?>
                     <div class="premium_section">
                         <div class="premium_section_inner">
                             <p>Become Premium member to view auction details and documents.</p>
                             <button class="btn btn-default" type="button" onclick="window.location='<?php echo base_url();?>home/premiumServices'">Premium Services</button>
-							<?php if(!$this->session->userdata('id')){ ?>
-								<p class="subscriber_para">If you are already a subscriber</p>
-								<ul class="login_register_section">
-									<li><a href="<?php echo base_url(); ?>home/login"><button class="btn btn-default btn_login" type="button">Login</button></a></li>
-									<li><p>Or</p></li>
-								<li><a href="<?php echo base_url(); ?>registration/signup"><button class="btn btn-default btn_register" type="button">Register</button></a></li>
-								</ul>
-							<?php } ?>
+                            <?php if(!$this->session->userdata('id')){ ?>
+                                <p class="subscriber_para">If you are already a subscriber</p>
+                                <ul class="login_register_section">
+                                    <li><a href="<?php echo base_url(); ?>home/login"><button class="btn btn-default btn_login" type="button">Login</button></a></li>
+                                    <li><p>Or</p></li>
+                                <li><a href="<?php echo base_url(); ?>registration/signup"><button class="btn btn-default btn_register" type="button">Register</button></a></li>
+                                </ul>
+                            <?php } ?>
                         </div>
                     </div>
-					<?php } ?>
+                    <?php } ?>
                 </div>
                 <div class="col-sm-3">
                     <div class="adblock_right">
@@ -288,37 +288,37 @@
                 </div>
             </div><!--row-->
         </div><!--container-fluid-->
-		<script>
-			function addtoeventfavlist(auctionID)
-			{
+        <script>
+            function addtoeventfavlist(auctionID)
+            {
 
-						
-						if (auctionID) {
-								var is_fav = $(".shortlist_list").hasClass('shortlisted_list');
-								if(is_fav)
-								{
-									$(".shortlist_list").removeClass('shortlisted_list');
-									$(".shortlist_list").html('<i class="fa fa-star"></i> Shortlist');
-								}
-								else
-								{
-									$(".shortlist_list").addClass('shortlisted_list');
-									$(".shortlist_list").html('<i class="fa fa-star"></i> Shortlisted');
-								}
-						jQuery.ajax({
-							url: '/home/liveupcomingauciton_event_add',
-							type: 'POST',
-							data: {
-								auctionID: auctionID,
-								message: "Added Successfully"
-							},
-							success: function (data) {
-								//alert(data);
-								//var rand = Math.random() * 10000000000000000;
-								//location.href = "?rand=" + rand;
-							}
-						});
-					}
-				
-			}
-		</script>
+
+                        if (auctionID) {
+                                var is_fav = $(".shortlist_list").hasClass('shortlisted_list');
+                                if(is_fav)
+                                {
+                                    $(".shortlist_list").removeClass('shortlisted_list');
+                                    $(".shortlist_list").html('<i class="fa fa-star"></i> Shortlist');
+                                }
+                                else
+                                {
+                                    $(".shortlist_list").addClass('shortlisted_list');
+                                    $(".shortlist_list").html('<i class="fa fa-star"></i> Shortlisted');
+                                }
+                        jQuery.ajax({
+                            url: '/home/liveupcomingauciton_event_add',
+                            type: 'POST',
+                            data: {
+                                auctionID: auctionID,
+                                message: "Added Successfully"
+                            },
+                            success: function (data) {
+                                //alert(data);
+                                //var rand = Math.random() * 10000000000000000;
+                                //location.href = "?rand=" + rand;
+                            }
+                        });
+                    }
+
+            }
+        </script>
