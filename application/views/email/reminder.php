13b7d9cbc575c220c61f977096c348a397a2c5ac
darpankumar1986 <58677039+darpankumar1986@users.noreply.github.com>
2020-08-27 13:52:34 +0530

    Update reminder.php

--- application/views/email/reminder.php
+++ application/views/email/reminder.php
@@ -1,5 +1,5 @@
 Dear <?php echo $first_name; ?>,<br/></br/>
-This is a reminder that you subscription at AuctionOnClick.com is about to expire on <?php echo date('F dS, Y',strtotime($package_end_date)); ?>.<br/></br/>
+This is a reminder that you subscription at <a href="<?php echo base_url(); ?>" style="text-decoration: none;color: #0078db;margin-bottom:10px;">AuctionOnClick.com</a> is about to expire on <?php echo date('F dS, Y',strtotime($package_end_date)); ?>.<br/></br/>
 To renew or upgrade your subscription, please login to <a href="<?php echo base_url();?>owner/manageSubscription">www.AuctionOnClick.com/premium-services</a><br/></br/>
 Regards,<br/>
 <a href="<?php echo base_url(); ?>" style="text-decoration: none;color: #0078db;margin-bottom:10px;">AuctionOnClick.com</a><br/>
