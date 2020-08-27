0215035523c23d2260e7066369ba8fb09eeedb03
akashsharma13 <58677901+akashsharma13@users.noreply.github.com>
2020-08-27 15:00:26 +0530

    reminder emailer
    reminder emailer

--- application/views/email/reminder.php
+++ application/views/email/reminder.php
@@ -1,4 +1,4 @@
-<p style="font-family: sans-serif;color: #333333;font-size: 14px;font-weight: normal;margin-top: 0;">Dear <?php echo $first_name; ?>,<br/></br/>
+<p style="font-family: sans-serif;color: #333333;font-size: 14px;font-weight: normal;margin-top: 0;margin-bottom: 0px !important;">Dear <?php echo $first_name; ?>,<br/></br/>
 This is a reminder that you subscription at <a href="<?php echo base_url(); ?>" style="text-decoration: none;color: #0078db;margin-bottom:10px;">AuctionOnClick.com</a> is about to expire on <?php echo date('F dS, Y',strtotime($package_end_date)); ?>.<br/></br/>
 To renew or upgrade your subscription, please login to <a href="<?php echo base_url();?>owner/manageSubscription">www.AuctionOnClick.com/premium-services</a><br/></br/>
 Regards,<br/>
